7bd10ae2662f2456dfba355001c52d1fea98fd8f
Joathan Theiller <jtheiller@hotmail.com>
2022-04-14 22:09:28 -0300

    Create: #4, Novo projeto sample para testar download via class com configuracoes de parametros da URL

---
+++ samples/ObjectRequest/Project1.dpr
@@ -0,0 +1,21 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  QuickChart in '..\..\src\QuickChart.pas',
+  QuickChart.Download in '..\..\src\QuickChart.Download.pas',
+  QuickChart.Request in '..\..\src\QuickChart.Request.pas';
+
+{$R *.res}
+
+begin
+  {$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := True;
+  {$ENDIF}
+
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ samples/ObjectRequest/Unit1.pas
@@ -0,0 +1,94 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Buttons,
+  Vcl.Samples.Spin;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    btnDownload: TBitBtn;
+    Memo1: TMemo;
+    Panel2: TPanel;
+    Image1: TImage;
+    Panel3: TPanel;
+    Label1: TLabel;
+    Panel4: TPanel;
+    Label2: TLabel;
+    Panel5: TPanel;
+    SpinEditWidth: TSpinEdit;
+    SpinEditHeigth: TSpinEdit;
+    Label3: TLabel;
+    Label4: TLabel;
+    SpinEditDevicePixelRatio: TSpinEdit;
+    Label5: TLabel;
+    EditBrackGroundColorHex: TEdit;
+    Label6: TLabel;
+    Label7: TLabel;
+    EditFileName: TEdit;
+    procedure btnDownloadClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses
+  QuickChart;
+
+{$R *.dfm}
+
+procedure TForm1.btnDownloadClick(Sender: TObject);
+const
+  JSON_TEXT ='{'
+            +'  "type":"bar",'
+            +'  "data":{'
+            +'    "labels":["January","February","March","April","May"],'
+            +'    "datasets":['
+            +'      {'
+            +'        "label":"Dogs",'
+            +'        "data":[50,60,70,180,190]'
+            +'      },'
+            +'      {'
+            +'        "label":"Cats",'
+            +'        "data":[100,200,300,400,500'
+            +'        ]'
+            +'      }'
+            +'    ]'
+            +'  }'
+            +'}';
+begin
+//  TQuickChart
+//    .New
+//      .SetWidth(500)
+//      .SetHeight(300)
+//      .SetDevicePixelRatio(2.0)
+//      .SetBackgroundColor('transparent')
+//      .SetFormat('png')
+//      .SetEncoding('url')
+//      .SetVersion('2.9.4')
+//      .SetChartInfo( JSON_TEXT )
+//      .Download( 'QuickChart.png' ) //Optional
+//    .Free;
+
+  TQuickChart
+    .New
+      .SetWidth(SpinEditWidth.Value)
+      .SetHeight(SpinEditHeigth.Value)
+      .SetDevicePixelRatio(SpinEditDevicePixelRatio.Value)
+      .SetBackgroundColor(EditBrackGroundColorHex.Text)
+      .SetChart( Memo1.Text )
+      .Download( Image1.Picture )   //Optional
+      .Download( EditFileName.Text )//Optional
+    .Free;
+end;
+
+end.
